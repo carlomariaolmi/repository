c49631ddc3d67fbb2b33e760618c9d0cff0c44b2
Carlo Olmi <carlomariaolmi@gmail.com>
2017-06-05 19:09:09 +0200

    Create ipython_tricks.py

---
+++ ipython_tricks.py
@@ -0,0 +1,16 @@
+###############################
+
+import numpy as np
+import pandas as pd
+
+###############################
+
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+%matplotlib inline
+
+###############################
+
+import seaborn as sns
+
+###############################
